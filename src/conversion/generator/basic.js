3f176d560bc69d4d5862c0c5bf03f43aab966caa
Ben Browning <bbrowning040518@gmail.com>
2025-11-01 16:43:39 +0000

    better basic

--- src/conversion/generator/basic.js
+++ src/conversion/generator/basic.js
@@ -1,65 +1,72 @@
-export class Basic 
-{
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-    SCHEMA_FILE = './src/conversion/schema/basic.json'
-    START_SYMBOL = '~'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-    constructor(alphabet, minHz = 400,  maxHz = 8000, bands = 50) 
-    {
-        this.alphabet = alphabet;
+export class Basic {
+  START_SYMBOL = '~';
 
-        if (this.alphabet.includes(this.START_SYMBOL)) {
-            throw new Error('Alphabet start symbol clash')
-        }
-        this.alphabet.push(this.START_SYMBOL)
-
-        this.minHz = minHz;
-        this.maxHz = maxHz;
-        this.bands = bands;
-    
-
-        // Create valid freque
-        this.valid_hz = [];
-
-        for (let i = 0; i < bands; i++) {
-            // Equal spacing across range
-            this.valid_hz.push(
-                minHz + Math.floor(
-                    (maxHz - minHz)  * (i / bands)
-                )
-            )
-        }
-
-        // Assign symbols frequency
-        const symbolCount = this.alphabet.length;
-        const soundsPerSymbol = Math.ceil(symbolCount / bands)
-
-        this.frequencyMap = this.alphabet.reduceRight((ret, cur, index) => {
-            ret[cur] = [];
-
-            let v = index
-
-            for (let i = 0; i < soundsPerSymbol; i++) {
-                ret[cur].push(this.valid_hz[v % bands])
-
-                v = Math.floor(v / bands);
-            }
-
-            return ret
-        }, {})
-        
+  constructor(alphabet, minHz = 400, maxHz = 8000, bands = 50) {
+    // Validate inputs
+    if (!Array.isArray(alphabet) || alphabet.length === 0) {
+      throw new Error('Alphabet must be a non-empty array');
     }
 
-    produceJson() {
-        const data = {
-            alphabet: this.alphabet,
-            valid_hz: this.valid_hz,
-            start: this.START_SYMBOL,
-            frequencyMap: this.frequencyMap
-        }
-
-        return JSON.stringify(data);
+    if (minHz >= maxHz) {
+      throw new Error('minHz must be less than maxHz');
     }
-    
 
+    if (alphabet.includes(this.START_SYMBOL)) {
+      throw new Error('Alphabet contains reserved start symbol');
+    }
+
+    // Don't mutate the input - create new array
+    this.alphabet = [...alphabet, this.START_SYMBOL];
+
+    this.minHz = minHz;
+    this.maxHz = maxHz;
+    this.bands = bands;
+
+    // Use path.join for reliable path handling
+    this.SCHEMA_FILE = join(__dirname, '../schema/basic.json');
+
+    // Create valid frequencies
+    this.valid_hz = [];
+
+    for (let i = 0; i < bands; i++) {
+      this.valid_hz.push(
+        Math.round(minHz + (maxHz - minHz) * (i / (bands - 1)))
+      );
+    }
+
+    // Assign symbols to frequency sequences
+    const symbolCount = this.alphabet.length;
+    const soundsPerSymbol = Math.ceil(Math.log(symbolCount) / Math.log(bands));
+
+    this.frequencyMap = this.alphabet.reduceRight((ret, cur, index) => {
+      ret[cur] = [];
+
+      let v = index;
+
+      for (let i = 0; i < soundsPerSymbol; i++) {
+        ret[cur].push(this.valid_hz[v % bands]);
+
+        v = Math.floor(v / bands);
+      }
+
+      return ret;
+    }, {});
+  }
+
+  produceJson() {
+    const data = {
+      alphabet: this.alphabet,
+      valid_hz: this.valid_hz,
+      start: this.START_SYMBOL,
+      frequencyMap: this.frequencyMap,
+    };
+
+    return JSON.stringify(data, null, 2); // Added pretty printing
+  }
 }
