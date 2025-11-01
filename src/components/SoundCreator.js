70224bdf38c1c29ecf249c6805ec35ee7eb148a5
Kai Vennemann <kai.an.vennemann@gmail.com>
2025-11-01 17:08:40 +0000

    integrated Ben's parser

--- src/components/SoundCreator.js
+++ src/components/SoundCreator.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import play from '../audio-output/play.js';
+import { convertFromTextToHz } from '../conversion/parser/basic.js';
 
 function playNote() {
+  const frequencyArray = convertFromTextToHz('hello world');
+  console.log(frequencyArray);
   // play([523, 659, 784, 1047], 0.3);
-  play([2000], 3.3);
+  play(frequencyArray, 3.3);
 }
 
 function SoundCreator({ className }) {
