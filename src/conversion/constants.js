54ba316c5b86d0b19b7f40ff28664b97ff06944e
Ben Browning <bbrowning040518@gmail.com>
2025-11-01 14:58:53 +0000

    alphabet constatns

---
+++ src/conversion/constants.js
@@ -0,0 +1,9 @@
+
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const numbers = '1234567890'.split('');
+const upperLetters = lowerLetters.map((l) => l.toUpperCase())
+const special = '-/.,#$%^&*()!\''
+
+export const alphabet = [
+
+]
